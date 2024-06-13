485d360170339f9bec03e49067536c7d9b9ac2d9
andiizhu <azhu0317@gmail.com>
2024-06-12 22:54:06 -0400

    finished

--- src/app.js
+++ src/app.js
@@ -1,16 +1,53 @@
 import {
-  renderStatus,
-  setupPageBasics,
-  renderUsers,
-  renderPosts,
-  renderNewUser,
-} from './render-functions.js';
+	renderStatus,
+	setupPageBasics,
+	renderUsers,
+	renderPosts,
+	renderNewUser,
+} from "./render-functions.js";
 import {
-  checkResponseStatus,
-  getUserPosts,
-  createNewUser,
-  getUsers
-} from './fetch-functions.js';
+	checkResponseStatus,
+	getUserPosts,
+	createNewUser,
+	getUsers,
+} from "./fetch-functions.js";
 
 export default function app(appDiv) {
+	const page = setupPageBasics(appDiv); // setup page.
+	checkResponseStatus().then((statusInfoObj) =>
+		renderStatus(page.statusDiv, statusInfoObj)
+	);
+	getUsers().then((users) => renderUsers(page.usersUl, users));
+
+	page.usersUl.addEventListener("click", (event) => {
+		if (event.target.tagName === "BUTTON") {
+			let userId = event.target.getAttribute("data-user-id");
+			getUserPosts(userId).then((posts) => renderPosts(page.postsUl, posts));
+		}
+	});
+
+	// page.newUserForm.addEventListener("submit", (event) => {
+	// 	event.preventDefault();
+	// 	const formData = new FormData(event.target);
+	// 	const formObject = Object.entries(formData);
+
+	// 	createNewUser(formObject).then((newUserInfo) =>
+	// 		renderNewUser(page.newUserDiv, newUserInfo)
+	// 	);
+	// 	event.target.reset();
+	// });
+
+	page.newUserForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		const formObject = {};
+		formData.forEach((value, key) => {
+			formObject[key] = value;
+		});
+
+		createNewUser(formObject).then((newUserInfo) =>
+			renderNewUser(page.newUserDiv, newUserInfo)
+		);
+		event.target.reset();
+	});
 }
--- src/render-functions.js
+++ src/render-functions.js
@@ -1,5 +1,5 @@
 export const setupPageBasics = (parentEl) => {
-    parentEl.innerHTML = `
+	parentEl.innerHTML = `
       <h1>Intro To Fetch!</h1>
       <div id='status'></div>
       <div id='users'>
@@ -21,23 +21,62 @@ export const setupPageBasics = (parentEl) => {
       <div id='new-user'></div>
     `;
 
-    const statusDiv = parentEl.querySelector('#status');
-    const usersUl = parentEl.querySelector('#users-list');
-    const postsUl = parentEl.querySelector('#posts-list');
-    const newUserForm = parentEl.querySelector('#new-user-form');
-    const newUserDiv = parentEl.querySelector('#new-user');
+	const statusDiv = parentEl.querySelector("#status");
+	const usersUl = parentEl.querySelector("#users-list");
+	const postsUl = parentEl.querySelector("#posts-list");
+	const newUserForm = parentEl.querySelector("#new-user-form");
+	const newUserDiv = parentEl.querySelector("#new-user");
 
-    return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
+	return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
 };
 
-export const renderStatus = () => {
-}
+export const renderStatus = (statusDiv, statusInfoObj) => {
+	const h2 = document.createElement("h2"); // create new h2 element
+	const p = document.createElement("p"); // create new p element
+	h2.id = "status-heading"; // create a new id attribute for h2
+	p.id = "status-code"; // create a new id attribute for p
+	h2.textContent = `Info on - ${statusInfoObj.url}`; // update readable text for h2
+	if (statusInfoObj.ok) {
+		// if the response ok is true
+		p.textContent = `Status code: ${statusInfoObj.status}, OK!`; // update the readable text for p
+	} else p.textContent = `Status code: ${statusInfoObj.status}, FAIL!`; //update the readable text for p
 
-export const renderUsers = () => {
+	statusDiv.append(h2, p); // add two child elements for h2 and p
 };
 
-export const renderPosts = () => {
-}
+export const renderUsers = (usersUl, users) => {
+	usersUl.innerHTML = ``; // clears the HTML for inside of postUL
+	users.forEach((postElement) => {
+		// for each element inside of users
+		const li = document.createElement("li"); // create a new li element
+		const button = document.createElement("button"); // create a new button element.
+		button.textContent = `Load ${postElement.username}'s posts`; // update the button readable text.
+		button.setAttribute("data-user-id", postElement.id); // create a new attribute for button with the name of "data-user-id" and a value of e.id
+		li.append(button); // make the button a child element of the li
+		li.classList.add("user-card"); // add a class to li
+		usersUl.append(li); // make the li a child element of usersUl
+	});
+};
 
-export const renderNewUser = () => {
-}
+export const renderPosts = (postsUl, posts) => {
+	postsUl.innerHTML = ""; // clears the HTML for inside of postUL
+	posts.forEach((postElement) => {
+		// for each element inside of posts
+		const li = document.createElement("li"); // create a new li element
+		const h2 = document.createElement("h2"); // create a new h2 element
+		const p = document.createElement("p"); // create a new p element
+		h2.textContent = postElement.title; // make the readable text of h2 equal to the title property of each element
+		p.textContent = postElement.body; // make the readable text of p equal to the body property of eac element.
+		li.append(h2, p); // make the h2 and p child elements of li
+		postsUl.append(li); // make the li a child element of postsUl
+	});
+};
+
+export const renderNewUser = (newUserDiv, newUserInfo) => {
+	newUserDiv.innerHTML = ""; // clears the HTML for inside of newUserDiv
+	const h2 = document.createElement("h2"); // create a new h2 element
+	const p = document.createElement("p"); // create a new p element
+	h2.textContent = newUserInfo.username; // make the readable text of h2 equal to the username property of newUserInfo
+	p.textContent = newUserInfo.email; // make the readable text of p equal to the email property of newUserInfo
+	newUserDiv.append(h2, p); // make the h2 and p child elements of newUserDiv
+};
